8004247c9ed98c4d73b73707961628ba6934fc65
Hung N Vo <hvo33@gatech.edu>
2019-09-16 15:16:33 -0400

    blaa

---
+++ FishProject/Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+    public float speed;
+    public float turnspeed;
+    void Start ()
+    {
+    }
+
+    void FixedUpdate ()
+    {
+        transform.Translate(0f,
+                            0f,
+                            speed*Input.GetAxis("Vertical")*Time.deltaTime); 
+        transform.Rotate(0f,
+                         turnspeed*Input.GetAxis("Horizontal")*Time.deltaTime,
+                         0f);
+    }
+}
